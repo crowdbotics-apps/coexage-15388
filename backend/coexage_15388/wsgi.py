6459eae22f057cd4e293223029006b0f0ae38549
Crowdbotics <team@crowdbotics.com>
2020-04-03 14:01:13 +0000

    Update models

--- backend/coexage_15388/wsgi.py
+++ backend/coexage_15388/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coexage_15388.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coexage_15388.settings")
 
 application = get_wsgi_application()
